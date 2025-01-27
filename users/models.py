3e75a5800a843d911632b26d77186bc3913c553e
Kimseongju7 <rlatj777@naver.com>
2025-01-28 04:25:03 +0900

    related_name과 같은 property를 추가했을 때

--- users/models.py
+++ users/models.py
@@ -23,3 +23,5 @@ class User(AbstractUser):
     gender = models.CharField(max_length=10, choices=GenderChoices.choices, )
     language = models.CharField(max_length=2, choices=LanguageChoices.choices, )
     currency = models.CharField(max_length=5, choices=CurrencyChoices.choices, )
+    # 아무런 영향이 없음
+    # rooms = ["room1", "room2", "room3", "room4", "room5", "room6", "room7", "room8", "room9", "room10", ]
