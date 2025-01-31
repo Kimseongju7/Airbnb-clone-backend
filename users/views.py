8815c1111e7d58e225285179a3b2e153ef39d5aa
Kimseongju7 <rlatj777@naver.com>
2025-01-31 23:50:33 +0900

    Assignment(5) - 수동 Django Rest Framework refactoring, user tweet serializer 통일

--- users/views.py
+++ users/views.py
@@ -3,8 +3,8 @@ from .models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
-from .serializers import UserTweetsSerializer
 from tweets.models import Tweet
+from tweets.serializers import TweetSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -14,5 +14,5 @@ def user_tweets(request, pk):
     except User.DoesNotExist:
         raise NotFound
     tweets = Tweet.objects.filter(user=pk)
-    serializer = UserTweetsSerializer(tweets, many=True)
+    serializer = TweetSerializer(tweets, many=True)
     return Response(serializer.data)
